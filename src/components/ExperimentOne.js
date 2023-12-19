514c525232126c5a88b3bc0408609c9c0b3bf550
Grigorii Belov <belovgrigory139@gmail.com>
2023-12-20 00:26:47 +0300

    modified:   src/App.css modified:   src/components/ExperimentOne.js

--- src/components/ExperimentOne.js
+++ src/components/ExperimentOne.js
@@ -8,10 +8,25 @@ function ExperimentOne() {
     setResult(prevResult => prevResult + 1)
   }
 
+  const decrease = () => {
+    setResult(prevResult => prevResult - 1)
+  }
+
+  const powerOfTwo = () => {
+    setResult(prevResult => Math.pow(2, prevResult))
+  }
+
+  const reset = () => {
+    setResult(0)
+  }
+
   return (
     <div>
-        <button onClick={increase}>increase</button>
-
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
+        <button onClick={powerOfTwo}>Power of Two</button>
+        <button onClick={reset}>Reset</button>
+        
         {result}
       
     </div>
